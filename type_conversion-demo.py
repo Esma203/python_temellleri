4d7d4cce042891e2367d7f7d15e58c4798a27516
Esma Uslu <esma.uslu@bil.omu.edu.tr>
2026-02-22 12:04:44 +0300

    alan ve çevre hesaplama

---
+++ type_conversion-demo.py
@@ -0,0 +1,11 @@
+# Yarıçapı verilen bir dairenin alanını ve çevresini hesaplayan program
+
+pi = 3.14
+
+r = float(input("yarıçap: "))
+
+alan = pi * r**2
+cevre = 2 * pi * r
+
+print("Dairenin Alanı:", alan)
+print("Dairenin Çevresi:", cevre)
