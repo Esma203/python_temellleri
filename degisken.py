ed4fb2a8fb73fc535267c4cc2a44e122d728d18a
Esma Uslu <esma.uslu@bil.omu.edu.tr>
2026-02-22 11:43:30 +0300

    bilgiler

---
+++ degisken.py
@@ -0,0 +1,9 @@
+# Müşteri Bilgileri
+musteriAdi = "Esma"
+musteriSoyadi = "Uslu"
+musteriCinsiyet = True # Kadın
+musteriTcKimlik = "12345678901"
+musteriDogumYili = "2005"
+musteriYasi = 2026 - int(musteriDogumYili)
+print(musteriYasi)
+
