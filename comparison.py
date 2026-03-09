78e1f00666a6eb5da93268bdb507c2c74df6aa8a
Esma Uslu <esma.uslu@bil.omu.edu.tr>
2026-03-09 22:22:42 +0300

    karşılaştırma operatörleri

---
+++ comparison.py
@@ -0,0 +1,49 @@
+# 1-Girilen 2 sayıdan hangisi büyüktür?
+#a = int(input('a: '))
+#b = int(input('b: '))
+
+#result = a > b
+
+
+# 2-Kullanıcıdan 2 vize (%60) ve final (%40) notunu alıp ortalama hesaplayınız.
+#  Eğer ortalama 50 ve üstündeyse geçti değilse kaldı yazdırın.
+#vize1 = float(input('1. vize: '))
+#vize2 = float(input('2. vize: '))
+#final = float(input('final: '))
+
+#ortalama = ((vize1 + vize2) / 2) * 0.6 + (final * 0.4)
+#print(f'not ortalamanız: {ortalama} ve durumunuz: {"geçti" if ortalama >= 50 else "kaldı"}')
+# 3-Girilen bir sayının tek mi çift mi olduğunu yazdırın.
+#sayi = int(input('sayı: '))
+
+#tekcift =(sayi % 2 == 0)
+
+#print(f'girilen sayı çift olma durumu: {tekcift}')
+
+# 4-Girilen bir sayının negatif pozitif durumunu yazdırın.
+#sayi = int(input('sayı: '))
+#pozitifmi = sayi > 0
+
+#print(f'girilen sayı pozitif olma durumu: {pozitifmi}')
+
+
+# 5-Parola ve email bilgisini isteyip doğruluğunu kontrol ediniz.
+#   (email: email@esmauslu.com parola: esma123)
+
+#email = 'email@esmauslu.com'
+#parola = 'esma123'
+#girilen_Email = input('email: ')
+#girilen_Parola = input('parola: ')
+
+#isEmail = (email == girilen_Email)
+#isParola = (parola == girilen_Parola)
+
+#print(f'email doğruluğu: {isEmail} parola doğruluğu: {isParola}')
+
+
+
+
+
+
+#print(ortalama)
+#print(result)
